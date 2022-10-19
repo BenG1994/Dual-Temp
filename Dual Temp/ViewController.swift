7bbb49e25e55fbba150dbc830b5f09a20029b171
Ben Gauger <bengauger@33.yahoo.com>
2022-10-19 14:05:25 +0200

    Initial Commit

---
+++ Dual Temp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dual Temp
+//
+//  Created by Ben Gauger on 10/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
