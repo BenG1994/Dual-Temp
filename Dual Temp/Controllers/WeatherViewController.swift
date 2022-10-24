c789450f558b71f20c5afe338025f860cadabcea
BenG1994 <113778995+BenG1994@users.noreply.github.com>
2022-10-24 13:54:49 +0200

    Everything except updating when units button pressed and background is working.

--- Dual Temp/Controllers/WeatherViewController.swift
+++ Dual Temp/Controllers/WeatherViewController.swift
@@ -33,14 +33,20 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var flagImage: UIImageView!
     
     var weatherManager = WeatherManager()
+    //    var weather = WeatherModel(conditionId: Int, cityName: String, temperature: Double, feels_like: Double, visibility: Int, humidity: Int, wind: Double, sunrise: Double, sunset: Double, timezone: Int, country: String, latitude: Double, longitude: Double)
+    
     //    var weatherManagerFahrenheit = WeatherManagerFahrenheit()
     let locationManager = CLLocationManager()
     //    let locationManagerFahrenheit = CLLocationManager()
     
-    
+    let userDefaults = UserDefaults()
     
     override func viewDidLoad() {
+        
+        
         super.viewDidLoad()
+        var flag = Flag(countryCode: "US")!
+        
         
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
@@ -50,6 +56,8 @@ class WeatherViewController: UIViewController {
         weatherManager.delegate = self
         searchTextField.delegate = self
         unitsChanged.layer.cornerRadius = 10
+        flagImage.image = flag.originalImage
+        timezoneLabel.text = "GMT"
         
         //        unitsChanged.setTitle("Metric", for: UIControl.State.normal)
     }
@@ -62,18 +70,41 @@ class WeatherViewController: UIViewController {
     @IBAction func unitsPressed(_ sender: UIButton) {
         if (sender.currentTitle == "Imperial") {
             sender.setTitle("Metric", for: UIControl.State.normal)
+            
+//            func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel){
+//                DispatchQueue.main.async {
+//                    var newVisibility = weather.visibility/1000
+//                    self.feelsLikeLabel.text = "\(weather.feelsLikeString)°C"
+//                    self.visibilityLabel.text = "\(newVisibility)km"
+//                    self.windSpeedLabel.text = "\(weather.windSpeedString)km/h"
+//                }
+//                
+//            }
         }else {
             sender.setTitle("Imperial", for: UIControl.State.normal)
+            
+//            func didUpdateWeatherFahrenheit(_weatherManager: WeatherManager, weather: WeatherModelFahrenheit){
+//                DispatchQueue.main.async {
+//                    var newVisibility = Double(weather.visibilityImperial) * 0.000621371192237
+//                    var visibilityStringImperialNew: String {
+//                        return String (format: "%.0f", newVisibility)
+//                    }
+//                    self.feelsLikeLabel.text = "\(weather.feelsLikeStringFahrenheit)°F"
+//                    self.windSpeedLabel.text = "\(weather.windSpeedStringImperial)mp/h"
+//                    self.visibilityLabel.text = "\(visibilityStringImperialNew)m"
+//                }
+//            }
         }
+        userDefaults.setValue(sender.currentTitle, forKey: "UnitsLabel")
         let units = sender.currentTitle!
         unitsChanged.setTitle(units, for: UIControl.State.normal)
         print (units)
         
-
-        self.viewDidLoad()
         
     }
 }
+
+
 
 //MARK: - CLLocationManagerDelegate
 
@@ -172,65 +203,65 @@ extension WeatherViewController: WeatherManagerDelegate {
             }
         }
     }
-
-
-func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
-    var newVisibility = weather.visibility/1000
-    let countryCode = weather.country
-    let flag = Flag(countryCode: countryCode)!
-    
-
-    let sunsetTimeStamp = Date(timeIntervalSince1970: weather.sunset)
-    let formatter = DateFormatter()
-    formatter.dateStyle = .none
-    formatter.timeZone = TimeZone(secondsFromGMT: weather.timezone)
-    formatter.timeStyle = .short
-    let formattedSunsetTime = formatter.string(from: sunsetTimeStamp)
-    print (formattedSunsetTime)
-    
-    let sunriseTimeStamp = Date(timeIntervalSince1970: weather.sunrise)
-    let formattedSunriseTime = formatter.string(from: sunriseTimeStamp)
-    print (formattedSunriseTime)
     
     
-    
-    let inputTimeZone = TimeZone(secondsFromGMT: weather.timezone)!
-    let timeZoneIdentifier = inputTimeZone.identifier
-    
-    print(timeZoneIdentifier)
-
-    
-
-    DispatchQueue.main.async {
-
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
+        var newVisibility = weather.visibility/1000
+        let countryCode = weather.country
+        let flag = Flag(countryCode: countryCode)!
         
-        self.temperatureLabel.text = "\(weather.temperatureString)°C"
-        self.timezoneLabel.text = timeZoneIdentifier
-
-        self.humidityLabel.text = "\(weather.humidity)%"
         
-        self.cityLabel.text = "\(weather.cityName)"
-        self.flagImage.image = flag.originalImage
-        self.conditionImageView.image = UIImage(systemName: weather.conditionName)
-        self.sunsetLabel.text = "\(formattedSunsetTime)"
-        self.sunriseLabel.text = "\(formattedSunriseTime)"
-    }
-    
-    if unitsChanged.currentTitle == "Metric"{
+        let sunsetTimeStamp = Date(timeIntervalSince1970: weather.sunset)
+        let formatter = DateFormatter()
+        formatter.dateStyle = .none
+        formatter.timeZone = TimeZone(secondsFromGMT: weather.timezone)
+        formatter.timeStyle = .short
+        let formattedSunsetTime = formatter.string(from: sunsetTimeStamp)
+        print (formattedSunsetTime)
+        
+        let sunriseTimeStamp = Date(timeIntervalSince1970: weather.sunrise)
+        let formattedSunriseTime = formatter.string(from: sunriseTimeStamp)
+        print (formattedSunriseTime)
+        
+        
+        
+        let inputTimeZone = TimeZone(secondsFromGMT: weather.timezone)!
+        let timeZoneIdentifier = inputTimeZone.identifier
+        
+        print(timeZoneIdentifier)
+        
+        
+        
         DispatchQueue.main.async {
-            self.feelsLikeLabel.text = "\(weather.feelsLikeString)°C"
-            self.feelsLikeImageView.image = UIImage(systemName: weather.conditionName)
-            self.visibilityLabel.text = "\(newVisibility)km"
-            self.visibilityImageView.image = UIImage(systemName: weather.visibilityStrength)
-           
-            self.windSpeedLabel.text = "\(weather.windSpeedString)km/h"
             
+            
+            self.temperatureLabel.text = "\(weather.temperatureString)°C"
+            self.timezoneLabel.text = timeZoneIdentifier
+            
+            self.humidityLabel.text = "\(weather.humidity)%"
+            
+            self.cityLabel.text = "\(weather.cityName)"
+            self.flagImage.image = flag.originalImage
+            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+            self.sunsetLabel.text = "\(formattedSunsetTime)"
+            self.sunriseLabel.text = "\(formattedSunriseTime)"
         }
+        
+        if unitsChanged.currentTitle == "Metric"{
+            DispatchQueue.main.async {
+                self.feelsLikeLabel.text = "\(weather.feelsLikeString)°C"
+                self.feelsLikeImageView.image = UIImage(systemName: weather.conditionName)
+                self.visibilityLabel.text = "\(newVisibility)km"
+                self.visibilityImageView.image = UIImage(systemName: weather.visibilityStrength)
+                
+                self.windSpeedLabel.text = "\(weather.windSpeedString)km/h"
+                
+            }
+        }
+        
     }
     
-}
-
-func didFailWithError(error: Error) {
-    print (error)
-}
+    func didFailWithError(error: Error) {
+        print (error)
+    }
 }
